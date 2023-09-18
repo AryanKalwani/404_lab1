a9d0c55fa4f0a623f50934377396306e6f88bfc8
aryankalwani <kalwani.aryan@gmail.com>
2023-09-17 23:37:52 -0700

    Modify script

--- script.py
+++ script.py
@@ -4,3 +4,7 @@ print(requests.__version__)
 
 r1 = requests.get("http://www.google.com")
 print(r1.status_code)
+
+r2 = requests.get("https://raw.githubusercontent.com/AryanKalwani/404_lab1/main/script.py")
+print(r2.text)
+
